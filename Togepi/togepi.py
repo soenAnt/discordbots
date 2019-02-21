69db96598a972b71af3ca798f636f4d7bac428a9
Antoine B <antoine.beiten@gmail.com>
2019-02-20 23:32:57 -0500

    pushing files necessary for hosting on heroku

--- Togepi/togepi.py
+++ Togepi/togepi.py
@@ -7,11 +7,9 @@ from discord import Game
 from discord.ext.commands import Bot
 
 BOT_PREFIX = ("?", "!")
-# Get at discordapp.com/developers/applications/me
-TOKEN = "TOKEN"
+TOKEN = "NTQ2ODU3MjY5ODY2OTIxOTk0.D0vYeA.MqQ3OPBGxxf_0r2xRzM9Y8yUbQU"  # Get at discordapp.com/developers/applications/me
 
 client = Bot(command_prefix=BOT_PREFIX)
-
 
 @client.command(name='8ball',
                 description="Answers a yes/no question.",
@@ -29,8 +27,7 @@ async def eight_ball(context):
         'Definitely',
         'I think so!',
     ]
-    await context.send(random.choice(possible_responses) + ", " + context.message.author.mention)
-
+    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
 
 @client.event
 async def on_message(message):
@@ -40,26 +37,31 @@ async def on_message(message):
 
     if message.content.startswith('!hello') or message.content.startswith('?hello'):
         msg = 'Hello {0.author.mention}'.format(message)
-        await message.channel.send(msg)
-    elif message.content.startswith('Im sad'):
+        await client.send_message(message.channel, msg)
+
+    elif message.content.startswith('im sad') or message.content.startswith('Im sad') or message.content.startswith("I'm sad"):
         msg = 'Dont be sad {0.author.mention}'.format(message)
-        await message.channel.send(msg)
-
-
+        await client.send_message(message.channel, msg)
+    
+    elif message.content.startswith('bye') or message.content.startswith('Bye') or message.content.startswith('bai'):
+        msg = 'Bye! {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+        
 @client.event
 async def on_ready():
     await client.change_presence(game=Game(name="with humans"))
     print("Logged in as " + client.user.name)
 
 
-# async def list_servers():
-#     await client.wait_until_ready()
-#     while not client.is_closed:
-#         print("Current servers:")
-#         for server in client.servers:
-#             print(server.name)
-#         await asyncio.sleep(600)
+
+async def list_servers():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        print("Current servers:")
+        for server in client.servers:
+            print(server.name)
+        await asyncio.sleep(600)
 
 
-# client.loop.create_task(list_servers())
+client.loop.create_task(list_servers())
 client.run(TOKEN)
